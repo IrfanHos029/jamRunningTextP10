e4d0ebd038cdae8094b3d3fa6b2ddc67362a545f
IrfanHos02 <houseirfan02@gmail.com>
2022-01-22 00:40:45 +0700

    DONE(menghilangkan Serial Print)

--- ResetTimer.ino
+++ ResetTimer.ino
@@ -3,11 +3,11 @@ void Reset(){
   int jam = now.hour();
   int menit = now.minute();
   int detik = now.second();
-Serial.print(jam);
-Serial.print(":");
-Serial.print(menit);
-Serial.print(":");
-Serial.println(detik);
+//Serial.print(jam);
+//Serial.print(":");
+//Serial.print(menit);
+//Serial.print(":");
+//Serial.println(detik);
 
   if(jam == 0 && menit == 0 && detik == 0){
     Buzzer(1);
